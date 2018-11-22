a0de7f9b3db7d9d26e9f1d2ced4f29e29dc2d62a
Lukasz Korycki <korycki.lukas@gmail.com>
2018-11-22 05:42:38 -0500

    up

--- src/main/scala/AMRules.scala
+++ src/main/scala/AMRules.scala
@@ -2,21 +2,22 @@ import scala.collection.mutable.ArrayBuffer
 
 class AMRules() {
 
-  var rules: ArrayBuffer[Rule] = ArrayBuffer(Rule(ArrayBuffer()))
-  var rulesStats: ArrayBuffer[RuleStatistics] = ArrayBuffer(RuleStatistics(0))
-
   val attrNum: Int = 10
+  val clsNum: Int = 10
+
+  var rules: ArrayBuffer[Rule] = ArrayBuffer(Rule(ArrayBuffer()))
+  var rulesStats: ArrayBuffer[RuleStatistics] = ArrayBuffer(RuleStatistics(0, ArrayBuffer.fill(clsNum)(0)))
 
   val EXT_MIN: Int = 100
   val DELTA: Double = 1 - 0.95
-  val R: Double = 100.0
+  val R: Double = 1.0
 
   def update(instance: Instance): Unit = {
     var covered = false
 
     for ((rule, ruleId) <- rules.drop(1).zipWithIndex) {
       if (isCovered(instance, rule)) {
-        updateStatistics(ruleId) // dist
+        updateStatistics(ruleId, instance.classLbl) // dist
         covered = true
 
         if (rulesStats(ruleId).count > EXT_MIN) {
@@ -26,7 +27,7 @@ class AMRules() {
     }
 
     if (!covered) {
-      updateStatistics(0)
+      updateStatistics(0, instance.classLbl)
 
       if (rulesStats(0).count > EXT_MIN) {
         expandRule(0)
@@ -45,8 +46,11 @@ class AMRules() {
     true
   }
 
-  def updateStatistics(ruleId: Int): Unit = {
-    rulesStats.update(ruleId, RuleStatistics(rulesStats(ruleId).count + 1))
+  def updateStatistics(ruleId: Int, classLbl: Int): Unit = {
+    rulesStats.update(ruleId, RuleStatistics(
+      rulesStats(ruleId).count + 1,
+      rulesStats(ruleId).clsCount.updated(classLbl, rulesStats(ruleId).clsCount(classLbl) + 1) // todo: count, min, max, for each class: mean, var, p(c); use NormalDistribution()
+    ))
   }
 
   def expandRule(ruleId: Int): Unit = {
@@ -57,10 +61,10 @@ class AMRules() {
 
     if (ruleEntropy > bound) {
       (0 to attrNum).foreach(aidx => { // dist
-        val localBestSplit = findBestSplit(aidx) // dist?
+        val attrBestSplit = findBestSplit(aidx) // dist?
 
-        if (localBestSplit._2 < bestSplit._2) {
-          bestSplit = localBestSplit
+        if (attrBestSplit._2 < bestSplit._2) {
+          bestSplit = attrBestSplit
         }
       })
 
@@ -79,7 +83,7 @@ class AMRules() {
   }
 
   def releaseStatistics(ruleId: Int): Unit = {
-    // todo
+    rulesStats.update(ruleId, RuleStatistics(0, ArrayBuffer.fill(clsNum)(0))) // todo: ok?
   }
 
   def predict(): Double = {
@@ -89,11 +93,11 @@ class AMRules() {
   def conditionResolver(instanceAtt: Double, relation: String, ruleVal: Double): Boolean = relation match {
     case ">" => instanceAtt > ruleVal
     case "=" => instanceAtt == ruleVal
-    case "<" => instanceAtt < ruleVal
+    case "<=" => instanceAtt < ruleVal
   }
 
   case class Condition(attributeIdx: Int, relation: String, value: Double)
   case class Rule(conditions: ArrayBuffer[Condition])
-  case class RuleStatistics(count: Int)
-  case class Instance(attributes: ArrayBuffer[Double], classLbl: Double)
+  case class RuleStatistics(count: Int, clsCount: ArrayBuffer[Int])
+  case class Instance(attributes: ArrayBuffer[Double], classLbl: Int)
 }
