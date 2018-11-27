50fbdd022bb14d459aa6df6a9f8caa4eb234ff5a
Lukasz Korycki <korycki.lukas@gmail.com>
2018-11-26 21:51:14 -0500

    State tmp removed

--- src/main/scala/pipes/base/Predictor.scala
+++ src/main/scala/pipes/base/Predictor.scala
@@ -1,21 +1,13 @@
 package pipes.base
 
-import input.{StreamHeader}
+import input.StreamHeader
 import model.SequentialAMR
 import org.apache.flink.api.common.functions.MapFunction
-import org.apache.flink.api.common.state.ListState
-import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
-import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
 import event.Event
 
-class Predictor(streamHeader: StreamHeader, extMin: Int) extends CheckpointedFunction with MapFunction[Event, Event] {
+class Predictor(streamHeader: StreamHeader, extMin: Int) extends MapFunction[Event, Event] {
 
-  private var rulesState: ListState[SequentialAMR] = _
-  private val rulesModel: SequentialAMR = new SequentialAMR(streamHeader, extMin)
-
-  override def snapshotState(functionSnapshotContext: FunctionSnapshotContext): Unit = {}
-
-  override def initializeState(context: FunctionInitializationContext): Unit = {}
+  private val rulesModel: SequentialAMR = new SequentialAMR(streamHeader, extMin) // todo: add state management
 
   override def map(instanceEvent: Event): Event = {
     if (!instanceEvent.getType.equals("Instance")) {
